17cf43bb91a7dd944e347511ae8e63d4c024b5da
Blake Mitchell <38139493+BlakeMitchell829@users.noreply.github.com>
2018-09-15 16:28:12 -0500

    Update liri.js

--- liri.js
+++ liri.js
@@ -1,44 +1,102 @@
 require("dotenv").config();
 
-let fs = require("fs");
-let spotify = require("./bin/spotifyAPI.js");
-let movies = require("./bin/movies.js");
-let twitter = require("./bin/twitter.js");
-//=============================================================================================================
+const Twitter = require("twitter");
+const Spotify = require("node-spotify-api");
+const keys = require("./keys");
+const request = require("request");
+const fs = require("fs");
+const spotify = new Spotify(keys.spotify);
 
-let args = process.argv.slice(2);
-
-function switcher(arg, arg2) {
-  if (args[1] !== undefined) {
-    arg2 = args[1];
-  };
-  switch (arg) {
-    case "spotify-this-song":
-    return spotify.spotifySearch(arg2);
-    break;
-    case "movie-this":
-    return movies.movieGet(arg2);
-    break;
-    case "my-tweets":
-    return twitter.tweetGet();
-    break;
-    case "do-what-it-says":
-    let randomTxt = function() {
-      const data = fs.readFileSync("./bin/random.txt", 'utf8');
-      let cmdArr;
-      if (data.includes(',')) {
-        cmdArr = data.trim().split(',');
-        process.argv.push(cmdArr[1]);
-        switcher(cmdArr[0], cmdArr[1]);
-      } else {
-        cmdArr = [data.trim()];
-        switcher(cmdArr[0]);
-      }
-    };
-    return randomTxt();
-    break;
+let getArtistName = (artist) => {
+  return artist.name;
+ };
+ let getSpotify = (songName) => {
+  if (songName === undefined) {
+    songName = "The Sign";
   }
-}
 
-switcher(args[0]);
+  spotify.search(
+    {
+      type: "track",
+      query: songName
+    },
+    (err, data)=> {
+      if (err) {
+        console.log("Error occurred: " + err);
+        return;
+      }
+      let songs = data.tracks.items;
 
+      for (let i = 0; i < songs.length; i++) {
+        console.log(i);
+        console.log("Artist: " + songs[i].artists.map(getArtistName));
+        console.log("Song Title: " + songs[i].name);
+        console.log("Preview: " + songs[i].preview_url);
+        console.log("Album: " + songs[i].album.name);
+        console.log("*   *   *   *   *   *");
+        fs.appendFile('log.txt', "\n" + "\n" + 'SPOTIFY THIS SONG: ' + i + "\n" + "Artist: " + songs[i].artists.map(getArtistName) + "\n" + "Song Title: " + songs[i].name + "\n" + "Preview: " + songs[i].preview_url + "\n" + "Album: " + songs[i].album.name +  "\n" + "Album: " + songs[i].album.name, (err) => {
+          if (err) throw err;
+        });
+      }
+    }
+  );
+};
+let myTweets = ()=> {
+ let client = new Twitter(keys.twitter);
+
+  let params = {
+    screen_name: "LevyTX"
+  };
+  client.get("statuses/user_timeline", params, (error, tweets, response) => {
+    if (!error) {
+      for (let i = 0; i < tweets.length; i++) {
+        console.log(tweets[i].created_at);
+        console.log("");
+        console.log(tweets[i].text);
+        fs.appendFile('log.txt', "\n" + "\n" + 'MY TWEETS: ' + "\n" + tweets[i].created_at + "\n" + tweets[i].text + "\n-----------", function (err) {
+          if (err) throw err;
+        });
+      }
+    }
+  });
+};
+let movieThis = (movieName) => {
+  if (movieName === undefined) {
+    movieName = "Crash";
+  }
+
+  let movieData = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=full&tomatoes=true&apikey=454a6e93";
+
+  request(movieData, (error, response, body) => {
+    if (!error && response.statusCode === 200) {
+      let jsonData = JSON.parse(body);
+
+      console.log("\nTitle: " + jsonData.Title);
+      console.log("\nYear: " + jsonData.Year);
+      console.log("\nRated: " + jsonData.Rated);
+      console.log("\nIMDB Rating: " + jsonData.imdbRating);
+      console.log("\nCountry: " + jsonData.Country);
+      console.log("\nLanguage: " + jsonData.Language);
+      console.log("\nPlot: " + jsonData.Plot);
+      console.log("\nActors: " + jsonData.Actors);
+      console.log("\nRotten Tomatoes Rating: " + jsonData.Ratings[1].Value);
+      fs.appendFile('log.txt', "\n" + "\n" + 'MOVIE THIS: ' + "\nTitle: " + jsonData.Title + "\nYear: " + jsonData.Year + "\nRated: " + jsonData.Rated + "\nIMDB Rating: " + jsonData.imdbRating + "\nCountry: " + jsonData.Country + "\nLanguage: " + jsonData.Language + "\nPlot: " + jsonData.Plot + "\nActors: " + jsonData.Actors + "\nRotten Tomatoes Rating: " + jsonData.Ratings[1].Value, (err) => {
+        if (err) throw err;
+      });
+    }
+  });
+};
+var doWhatItSays = function() {
+  //node modul reads texts from random.tx 
+  fs.readFile("random.txt", "utf8", function(error, data) {
+    console.log(data);
+  //splits data into two arrays at the ","
+    var dataArr = data.split(",");
+    if (dataArr.length == 2) {
+      pick(dataArr[0], dataArr[1]);
+    }
+    else if (dataArr.length == 1) {
+      pick(dataArr[0]);
+    }
+  });
+};
